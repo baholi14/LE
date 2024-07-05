af3d5c051429c09356457e0d2c29bf5534f11e94
Brian Lipesa <blipesa@dtbafrica.com>
2024-07-05 15:08:19 +0300

    Changed the app.run() line in app.py to bind to 0.0.0.0 & specified the port.

--- life-exp-poc/app/api/app.py
+++ life-exp-poc/app/api/app.py
@@ -43,5 +43,6 @@ def predict_life_expectancy():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 
